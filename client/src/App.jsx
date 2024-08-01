74f7237262cd1af7e2826fd1ea1c0b0d39d00eab
Ifeoluwapo Adesoji <ayobamiadesoji2@gmail.com>
2024-08-01 17:20:01 +0100

    Frontend Installation

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+        <h1 className='font-sans text-6xl font-extrabold'>CONNECTIX</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
